7abe7955afb259b89bc4ce2b2d03cf51342e1826
Elizabete <elizabetesilvaoliveira.liz@gmail.com>
2025-04-23 09:31:56 -0300

    Projeto React

---
+++ src/componentes/Footer/index.jsx
@@ -0,0 +1,10 @@
+import styles from './footer.module.css'
+
+function footer() {
+    return (
+        <footer className={styles.footer}>
+            Turminha 2DS 2025 do Will
+            </footer>
+    )
+}
+export default footer
